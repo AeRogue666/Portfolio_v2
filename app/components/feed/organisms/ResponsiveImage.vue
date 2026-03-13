e5af165744dc0610dd110bddd8392e9291548e57
AurelDev <38510448+AeRogue666@users.noreply.github.com>
2026-03-13 17:36:38 +0100

    Updating ResponsiveImage rendering - 1

--- app/components/feed/organisms/ResponsiveImage.vue
+++ app/components/feed/organisms/ResponsiveImage.vue
@@ -24,9 +24,18 @@ const src = props.image.sources.feed?.mobile || props.image.sources.detail?.mobi
 
 <template>
     <div class="relative overflow-hidden rounded-xl shadow-md">
-        <NuxtImg :src="src" :alt="image.alt" sizes="(min-width: 80rem) 64rem, (min-width: 64rem) 80vw, 100vw"
-            :srcset="` ${src} 640w, ${tabletSrc} 768w, ${desktopSrc} 1024w`"
+        <!-- Simple NuxtImg -->
+        <NuxtImg :src="src" :alt="image.alt"
             class="w-full h-auto object-cover transition-transform duration-300 hover:scale-105" loading="lazy" />
+        <!-- Sizes NuxtImg -->
+        <NuxtImg :src="src" :alt="image.alt" sizes="(min-width: 80rem) 64rem, (min-width: 64rem) 80vw, 100vw"
+            class="w-full h-auto object-cover transition-transform duration-300 hover:scale-105" loading="lazy" />
+        <!-- Srcset NuxtImg -->
+        <NuxtImg :src="src" :alt="image.alt" :srcset="` ${src} 640w, ${tabletSrc} 768w, ${desktopSrc} 1024w`"
+            class="w-full h-auto object-cover transition-transform duration-300 hover:scale-105" loading="lazy" />
+        <!-- <NuxtImg :src="src" :alt="image.alt" sizes="(min-width: 80rem) 64rem, (min-width: 64rem) 80vw, 100vw"
+            :srcset="` ${src} 640w, ${tabletSrc} 768w, ${desktopSrc} 1024w`"
+            class="w-full h-auto object-cover transition-transform duration-300 hover:scale-105" loading="lazy" /> -->
     </div>
 </template>
 
